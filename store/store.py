8c08329a686487070d6ceab130a105ad770c1072
Stephen Tyndall <styndall@deepfield.net>
2016-04-22 13:49:10 -0400

    Added storage library directory and initial file

---
+++ store/store.py
@@ -0,0 +1,5 @@
+'''
+This file contains storage and retrieval methods for corpus data.
+we can use mysql or just local storage in a directory, or maybe even amazon s3
+for extra data security. Run this by everyone. Talk to Batia.
+'''
